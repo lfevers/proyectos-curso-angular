e58d52b7dde3daae71f2372dd7aea4c18feda4fd
Unknown <sergiopatohumanes@gmail.com>
2018-11-08 00:32:13 +0100

    Fin sección 5

--- 3-appPipes/src/app/pipes/contrasena.pipe.ts
+++ 3-appPipes/src/app/pipes/contrasena.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ContrasenaPipe implements PipeTransform {
 
-  transform(value: string, activar: boolean): string {
+  transform(value: string, activar: boolean = true): string {
 
+    let tam = value.length;
+
+    if(activar){
+      value = "";
+      for (let i = 0; i < tam; i++) {
+          value = value + "*";
+      }
+
+    }
     return value;
   }
 
